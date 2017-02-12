cfe1beb5011ca69f3ba45d3a39022b040d4d301b
Dan Newman <dan@newman.is>
2017-02-12 18:36:30 +1300

    dan.newman.is
    Old files and content from my dan.newman.is site.

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+<title>Dan Newman &#8211; Designer</title>
+
+<meta name="description" content="Dan Newman is an identity &amp; interactive designer from Wellington, New Zealand."/>
+
+<meta name="author" content="Dan Newman" />
+<meta name="copyright" content="Copyright Dan Newman, <?php echo date("Y"); ?>" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="/filing/css/styles.css">
+
+<script src="/filing/js/libs/modernizr-2.5.3.min.js"></script>
+
+</head>
+
+<body>
+
+
+  <div class="intro">
+    
+    <h1><span>Dan Newman</span> is a designer.</h1>
+    
+    <p>You can find me tweeting on <a class="twitter" href="http://www.twitter.com/danserif">Twitter</a>, applying filters to photos on <a class="instagram" href="http://www.instagram.com/danserif/">Instagram</a>, listening to music on <!--<a class="rdio" href="http://www.rdio.com/people/danserif/">Rdio</a>--><a class="spotify" href="https://open.spotify.com/user/danserif">Spotify</a>, <!--<strike>checking in on <a class="foursquare" href="http://foursquare.com/danserif">Foursquare</a></strike>, -->dribbbling shots on <a class="dribbble" href="http://www.dribbble.com/danserif">Dribbble</a>, pinning things of interest on <a class="pinterest" href="http://www.pinterest.com/danserif">Pinterest</a>, tracking films on <a class="letterboxd" href="http://www.letterboxd.com/danserif">Letterboxd</a>, writing words on <a class="medium" href="http://medium.com/@danserif">Medium</a>, copying and pasting code from <a class="github" href="http://github.com/danserif">Github</a>, being serious on <a class="linkedin" href="http://www.linkedin.com/in/danserif">LinkedIn</a> and less so on <a class="facebook" href="http://www.facebook.com/danserif">Facebook</a>.</p>
+    
+
+    
+    
+    <p>I live in <a class="wellington" href="http://en.wikipedia.org/wiki/Wellington">Wellington</a>, and work at <a class="koordinates" href="http://koordinates.com">Koordinates</a>. <!--I was previously Creative Director at <a class="xero" href="http://www.xero.com">Xero</a>.--></p>
+    
+    
+    <p>You can get in touch by <a href="mailto:dan@newman.is">Email</a>.</p>
+    
+    <div class="links">
+	  
+	  <!--<p><span class="new">NEW</span> <a class="site" href="http://www.flagdesign.nz">flagdesign.nz</a> — Documenting the redesign of the New Zealand flag.</p>-->
+	  <p><span class="new">ElSEWHERE</span> <a class="site" href="http://www.flagdesign.nz">flagdesign.nz</a> + <a class="site" href="http://www.flagtest.nz">flagtest.nz</a></p>
+	  
+  	</div>
+    
+  </div>
+
+    
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+<script src="/filing/js/plugins.js"></script>
+<script src="/filing/js/script.js"></script>
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33585420-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+
+</body>
+</html>
